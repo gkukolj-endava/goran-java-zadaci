b64c85768c00ac90944311a5430970086f7fe37f
jana.djordjevic <jana.djordjevic@endava.com>
2018-10-02 16:07:10 +0200

    HelloWorld

---
+++ src/Zadatak1/HelloWorld.java
@@ -0,0 +1,18 @@
+package Zadatak1;
+
+/**
+ * 
+ * @author goran.kukolj
+ *
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		// prints out the string
+		System.out.println("Hello World");
+
+	}
+
+}
