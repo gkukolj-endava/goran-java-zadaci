6daa8d87dd6634eb721ef36db7c1bc388e4f05a3
gkukolj-endava <goran.kukolj@endava.com>
2018-10-04 13:29:23 +0200

    Ispravljene greske

--- src/Zadatak2/PogadjanjeSlucajnogBroja.java
+++ src/Zadatak2/PogadjanjeSlucajnogBroja.java
@@ -22,84 +22,91 @@ import java.util.Scanner;
  */
 public class PogadjanjeSlucajnogBroja {
 
+	private static Scanner unos;
+
 	public static void main(String[] args) {
 
 		// kreiramo random objekat da dobijemo random zamisljeni broj
 		Random random = new Random();
 		System.out.println("Dobrodosli u igru pogadjanja");
-		boolean novaIgra = true;
+		unos = new Scanner(System.in);
+		// kreiramo slucajan broj izmedju 1 i 50
+		int slucajanBroj = random.nextInt(50) + 1;
+		boolean nijeBlizu = false;
 
-		// pocinje igra u petlji i ponavlja se koliko hocemo puta
-		while (novaIgra) {
-
-			// kreiramo slucajan broj izmedju 1 i 50
-			int slucajanBroj = random.nextInt(49) + 1;
-			int brojKorisnika = 0;
-			boolean nijeBlizu = false;
+		// petlja same igre, ponavlja se dok ne pogodi broj ili bude blizu zamisljenog
+		// broja +/-5
+		do {
 
 			// korisnik unosi broj i dodeljujemo taj broj varijabli
 			System.out.println("Unesite broj od 1 - 50");
-			brojKorisnika = unosBroja();
+			int brojKorisnika = unosBroja();
 
-			// petlja same igre, ponavlja se dok ne pogodi broj ili bude blizu zamisljenog
-			// broja +/-5
-			do {
-				if (proveraPogotka(brojKorisnika, slucajanBroj) == 2) {
-					System.out.println("Promasili ste, probajte drugi broj...");
-					brojKorisnika = unosBroja();
-				} else if (proveraPogotka(brojKorisnika, slucajanBroj) == 1) {
-					System.out.println("Bili ste blizu!");
-					System.out.println("Zamisljeni broj je bio: " + slucajanBroj);
-					nijeBlizu = true;
-				} else if (proveraPogotka(brojKorisnika, slucajanBroj) == 0) {
-					System.out.println("Pogodili ste!");
-					nijeBlizu = true;
-				}
-			} while (!nijeBlizu);
+			// varijabli dodeljujemo rezultat metode
+			int rezultatProverePogotka = proveraPogotka(brojKorisnika, slucajanBroj);
 
-			// provera da li korisnik zeli ponovo da igra
-			System.out.print("Unesite \'ne\' za exit,a bilo sta drugo da ponovo igrate");
-			String ponovoIgrati = unosOdgovora();
-			if (ponovoIgrati.equalsIgnoreCase("ne")) {
-				novaIgra = false;
-				System.out.println("Hvala sto ste igrali nasu igru");
+			if (rezultatProverePogotka == 2) {
+				System.out.println("Promasili ste, probajte drugi broj...");
+				brojKorisnika = unosBroja();
+			} else if (rezultatProverePogotka == 1) {
+				System.out.println("Bili ste blizu! \n Zamisljeni broj je bio: " + slucajanBroj);
+				nijeBlizu = true;
+			} else if (rezultatProverePogotka == 0) {
+				System.out.println("Pogodili ste!");
+				nijeBlizu = true;
 			}
-		}
+			if (nijeBlizu == true) {
+				// provera da li korisnik zeli ponovo da igra
+				System.out.print("Unesite \'ne\' za exit,a bilo sta drugo da ponovo igrate");
+				String ponovoIgrati = unosOdgovora();
+				if (ponovoIgrati.equalsIgnoreCase("ne")) {
+					System.out.println("Hvala sto ste igrali nasu igru");
+				}
+			}
+		} while (!nijeBlizu);
+
+
+
+		unos.close();
 	}
 
 	// metod za unos broja od strane korisnika
-	public static int unosBroja() {
-		Scanner unos = new Scanner(System.in);
-		while (!unos.hasNextInt()) {
-			unos.next();
-			System.out.println("Unesite ceo broj izmedju 1 i 50...");
+	private static int unosBroja() {
+
+		int a = 0;
+		boolean logic = true;
+		while (logic) {
+			try {
+				a = Integer.parseInt(unos.next());
+
+				if (a < 1 || a > 50) {
+					System.out.println("Niste uneli ceo broj izmedju 1 i 50, pokusajte ponovo");
+					a = unos.nextInt();
+				} else
+					logic = false;
+			} catch (InputMismatchException ex) {
+				System.out.println("Niste uneli broj: " + ex.getMessage()
+						+ ", niste uneli ceo broj izmedju 1 i 50, pokusajte ponovo");
+			}
+			catch (NumberFormatException ex) {
+				System.out.println("Niste uneli broj: " + ex.getMessage()
+						+ ", niste uneli ceo broj izmedju 1 i 50, pokusajte ponovo");
+			}
 		}
-		return unos.nextInt();
+		return a;
 	}
 
 	// metod za unos odgovora od strane korisnika
-	public static String unosOdgovora() {
-		Scanner unos = new Scanner(System.in);
-		String unosKorisnika = "";
-		while (!unos.hasNext()) {
-			unos.next();
-			System.out.println("Unesite da ili ne...");
+	private static String unosOdgovora() {
+			return unos.next();
 		}
-		try {
-			unosKorisnika = unos.next();
-		} catch (InputMismatchException e) {
-			System.out.println(e);
-		}
-		return unosKorisnika;
-	}
 
 	// proverava da li je broj pogodjen
-	public static int proveraPogotka(int unetBroj, int slucajanBroj) {
+	private static int proveraPogotka(int unetBroj, int slucajanBroj) {
 
 		if (unetBroj == slucajanBroj) {
 			return 0;
-		} else if ((unetBroj - slucajanBroj <= 5 && unetBroj - slucajanBroj > 0)
-				|| (slucajanBroj - unetBroj <= 5 && slucajanBroj - unetBroj > 0)) {
+		} else if (Math.abs(unetBroj - slucajanBroj) <= 5) {
 			return 1;
 		} else
 			return 2;
